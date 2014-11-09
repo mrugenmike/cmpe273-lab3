a16515dda5ce68942223d1a023484dacb8e3c37f
Mrugen Deshmukh <mrugenmike@gmail.com>
2014-11-09 03:22:11 -0800

    Initial checkin for lab3

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,11 +1,26 @@
 package edu.sjsu.cmpe.cache.client;
 
+
+import java.util.Arrays;
+import java.util.List;
+import com.google.common.hash.*;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+        //ConsistentHash consistentHash = new ConsistentHash<String>(Hashing.md5(),4);
+        CacheServiceInterface cache = new DistributedCacheService("http://localhost:3000");
+        CacheServiceInterface cache3001 = new DistributedCacheService("http://localhost:3001");
+        CacheServiceInterface cache3002 = new DistributedCacheService("http://localhost:3002");
+        List<CacheServiceInterface> servers = Arrays.asList(cache, cache3001, cache3002);
+        //Hashing.consistentHash(Hashing.md5().hashString())
+
+
+
+        //servers.get(consistentHash.)
+
+
 
         cache.put(1, "foo");
         System.out.println("put(1 => foo)");
@@ -13,7 +28,23 @@ public class Client {
         String value = cache.get(1);
         System.out.println("get(1) => " + value);
 
+        cache3001.put(2,"bob");
+        System.out.println("get(2) from 3002 => "+cache3001.get(2));
+
         System.out.println("Existing Cache Client...");
     }
 
 }
+
+/*
+* 1 => a
+2 => b
+3 => c
+4 => d
+5 => e
+6 => f
+7 => g
+8 => h
+9 => i
+10 => j
+* */
---
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/ConsistentHash.java
@@ -0,0 +1,53 @@
+package edu.sjsu.cmpe.cache.client;
+
+import com.google.common.hash.HashFunction;
+
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class ConsistentHash<T> {
+    private final HashFunction hashFunction;
+    private final int numberOfReplicas;
+    private final SortedMap<Integer, T> circle =
+            new TreeMap<Integer, T>();
+
+    public ConsistentHash(HashFunction hashFunction,
+                          int numberOfReplicas, Collection<T> nodes) {
+
+        this.hashFunction = hashFunction;
+        this.numberOfReplicas = numberOfReplicas;
+
+        for (T node : nodes) {
+            add(node);
+        }
+    }
+
+    public void add(T node) {
+        for (int i = 0; i < numberOfReplicas; i++) {
+            circle.put(hashFunction.hashString(node.toString() + i).asInt(),
+                    node);
+        }
+    }
+
+    public void remove(T node) {
+        for (int i = 0; i < numberOfReplicas; i++) {
+            circle.remove(hashFunction.hashString(node.toString() + i).asInt());
+        }
+    }
+
+    public T get(Integer key) {
+        if (circle.isEmpty()) {
+            return null;
+        }
+        int hash = hashFunction.hashString(key.toString()).asInt();
+        if (!circle.containsKey(hash)) {
+            SortedMap<Integer, T> tailMap =
+                    circle.tailMap(hash);
+            hash = tailMap.isEmpty() ?
+                    circle.firstKey() : tailMap.firstKey();
+        }
+        return circle.get(hash);
+    }
+
+}
